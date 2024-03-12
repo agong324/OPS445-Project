e64078549ef0cf89b87842a26cd06e58283d67dd
agong324 <agong3@myseneca.ca>
2024-03-12 02:01:31 +0000

    added signup, logout. idk if sign in works yet

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from .forms import SignUpForm, LoginForm
 
 # Create your views here.
 def signup(request):
@@ -13,3 +15,26 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
+
+def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('core:home')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = authenticate(email=email, password=password)
+            if user:  # If the returned object is not None
+                login(request, user)  #connect the user
+                return redirect('core:home')
+            else:  # otherwise an error will be displayed
+                messages.error(request, 'Invalid email or password')
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse('users:login'))
