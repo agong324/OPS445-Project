c395f681259529ffab2048368aa7cf6ff1d551a2
agong324 <agong3@myseneca.ca>
2024-03-18 00:25:48 +0000

    password change optimized

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from .views import signup, log_in, log_out, profile, edit_profile
 from django.contrib.auth.views import (
     PasswordResetView,
     PasswordResetDoneView,
     PasswordResetConfirmView,
-    PasswordResetCompleteView
+    PasswordResetCompleteView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
 )
 
 
@@ -34,5 +36,12 @@ urlpatterns = [
         	template_name='users/password_reset_complete.html'),
         	name='password_reset_complete'
     	),
+    	path('password_change/', PasswordChangeView.as_view(
+        	template_name='users/password_change.html',
+        	success_url=reverse_lazy('users:password_change_done')),
+        	name='password_change'),
+    	path('password_change/done/', PasswordChangeDoneView.as_view(
+        	template_name='users/password_change_done.html'),
+        	name='password_change_done'),
 ]
 
